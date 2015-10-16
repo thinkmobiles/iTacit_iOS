bed1b16b1663c0024e36c18df76f773c688aad23
fueg0 <fueg0@ukr.net>
2015-10-16 14:22:48 +0300

    UserTableViewCell prototype

--- iTacit/Controllers/NewsViewController.swift
+++ iTacit/Controllers/NewsViewController.swift
@@ -11,3 +11,26 @@ import UIKit
 class NewsViewController: UIViewController {
 	
 }
+
+// MARK: - UITableViewDelegate
+
+extension NewsViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension NewsViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("NewsTableViewCell", forIndexPath: indexPath)
+        
+        return cell
+    }
+}
