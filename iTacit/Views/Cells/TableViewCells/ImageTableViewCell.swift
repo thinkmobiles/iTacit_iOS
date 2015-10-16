8d686c347d7b0afd801a339784fe5f457b0620dc
fueg0 <fueg0@ukr.net>
2015-10-16 17:39:27 +0300

    aded ImageTableViewCell

---
+++ iTacit/Views/Cells/TableViewCells/ImageTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  NewsDetailImageTableViewCell.swift
+//  iTacit
+//
+//  Created by MaksymRachytskyy on 10/16/15.
+//  Copyright © 2015 iTacit. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var mewsImageView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
