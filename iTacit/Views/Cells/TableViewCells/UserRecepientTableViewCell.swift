2e3608e71764e06d05c0cb5f5a9121445528d426
fueg0 <fueg0@ukr.net>
2015-11-05 13:15:12 +0200

    added ComposeReportsHeaderView, UserRecepientTableViewCell, ComposerRecepientsViewController

---
+++ iTacit/Views/Cells/TableViewCells/UserRecepientTableViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  UserRecepientTableViewCell.swift
+//  iTacit
+//
+//  Created by MaksymRachytskyy on 11/4/15.
+//  Copyright © 2015 iTacit. All rights reserved.
+//
+
+import UIKit
+
+enum UserRecepientCellType {
+    case Default, Interactive
+}
+
+class UserRecepientTableViewCell: UITableViewCell {
+    
+    private struct Constants {
+        static let defaultLeadingSize: CGFloat = 35.0
+        static let interactiveLeadingSize: CGFloat = 19.0
+        static let closeButtonWidth: CGFloat = 20.0
+    }
+
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userCredentialLabel: UILabel!
+    @IBOutlet weak var userDescriptionLabel: UILabel!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var userImageLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var closeButtonWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var separatorView: UIView!
+    
+    var delegate: ComposerHeaderViewDelegate?
+    
+    var cellType: UserRecepientCellType? {
+        didSet {
+            if let type = cellType {
+                switch type {
+                case .Default: prepareCellLikeDefault()
+                case .Interactive: prepareCellLikeInteractive()
+                }
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    @IBAction func closeButtonAction(sender: UIButton) {
+        delegate?.didSelectCloseButtonInHeaderView(self)
+    }
+    
+    private func prepareCellLikeDefault() {
+        userImageLeadingConstraint.constant = Constants.defaultLeadingSize
+        closeButton.hidden = true
+        closeButtonWidthConstraint.constant = 0
+        separatorView.hidden = true
+    }
+    
+    private func prepareCellLikeInteractive() {
+        userImageLeadingConstraint.constant = Constants.interactiveLeadingSize
+        closeButton.hidden = false
+        closeButtonWidthConstraint.constant = Constants.closeButtonWidth
+        separatorView.hidden = false
+    }
+}
---
+++ iTacit/Views/HeaderViews/ComposeReportsHeaderView.swift
@@ -0,0 +1,74 @@
+//
+//  ComposeReportsHeaderView.swift
+//  iTacit
+//
+//  Created by MaksymRachytskyy on 11/5/15.
+//  Copyright © 2015 iTacit. All rights reserved.
+//
+
+import UIKit
+
+protocol ComposerHeaderViewDelegate {
+    func didSelectExpandButtonInHeaderView(view: UIView)
+    func didSelectCloseButtonInHeaderView(view: UIView)
+}
+
+class ComposeReportsHeaderView: UIView {
+    
+    private struct Constants {
+        static let NibName = "ComposeReportsHeaderView"
+    }
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var viewContainer: UIView!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var expandImageView: UIImageView!
+    
+    var delegate: ComposerHeaderViewDelegate?
+    var section: Int?
+    var rowCount: String {
+        get {
+            return numberLabel.text ?? ""
+        }
+        set {
+            numberLabel.text = newValue
+        }
+    }
+    var isExpanded = false {
+        didSet {
+            if isExpanded {
+                expandImageView.image = UIImage(assetsIndetifier: .TrianglePointerUp)
+            } else  {
+                expandImageView.image = UIImage(assetsIndetifier: .TrianglePointerDown)
+            }
+        }
+    }
+    
+    // MARK: -  Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib()
+    }
+    
+    // MARK: - Private
+    
+    func loadViewFromNib() {
+        if let view = NSBundle.mainBundle().loadNibNamed(Constants.NibName, owner: self, options: nil).first as? UIView {
+            self.addSubview(view)
+        }
+    }
+    
+    @IBAction func closeButtonAction(sender: UIButton) {
+        delegate?.didSelectCloseButtonInHeaderView(self)
+    }
+
+    @IBAction func expandButtonAction(sender: UIButton) {
+        delegate?.didSelectExpandButtonInHeaderView(self)
+    }
+}
