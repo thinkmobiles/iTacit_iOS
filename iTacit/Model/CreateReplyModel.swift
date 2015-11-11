2518041e88384e658f47fbf6cc1b56de25bf619f
fueg0 <fueg0@ukr.net>
2015-11-11 17:56:06 +0200

    added CreateReplyModel(in progress)

---
+++ iTacit/Model/CreateReplyModel.swift
@@ -0,0 +1,18 @@
+//
+//  CreateReplyModel.swift
+//  iTacit
+//
+//  Created by MaksymRachytskyy on 11/11/15.
+//  Copyright © 2015 iTacit. All rights reserved.
+//
+
+import Foundation
+
+class CreateReplyModel {
+    
+    var messageId = ""
+    var body = ""
+    var senderID = ""
+    var privateYn = false
+    
+}
